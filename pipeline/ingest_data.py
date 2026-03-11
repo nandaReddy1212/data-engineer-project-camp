d898b023f1d340c05712cdbfb9ff023b612d3a0a
nandaReddy1212 <81372397+nandaReddy1212@users.noreply.github.com>
2026-03-11 05:27:48 +0000

    used click to pass arguments in command line

--- pipeline/ingest_data.py
+++ pipeline/ingest_data.py
@@ -3,7 +3,6 @@ from tqdm.auto import tqdm
 from sqlalchemy import create_engine
 import click
 
-
 dtype = {
     "VendorID": "Int64",
     "passenger_count": "Int64",
@@ -29,20 +28,28 @@ parse_dates = [
 ]
 
 
-def run():
-    year = 2021
-    month = 1
-    pg_user = 'root'
-    pg_pass = 'root'
-    pg_host = 'localhost'
-    pg_port = 5432
-    pg_database = 'ny_taxi'
-    chunksize = 10000
-    name='yellow_taxi_data'
-
+@click.command()
+@click.option("--year", prompt="year", default=2021, type=int,
+              help="year of the dataset")
+@click.option("--month", prompt="month", default=1, type=int,
+              help="month of the dataset")
+@click.option("--pg-user", prompt="Postgres user", default="root")
+@click.option("--pg-pass", prompt="Postgres password", default="root",
+              hide_input=True)
+@click.option("--pg-host", prompt="Postgres host", default="localhost")
+@click.option("--pg-port", prompt="Postgres port", default=5432, type=int)
+@click.option("--pg-database", prompt="Postgres database", default="ny_taxi")
+@click.option("--chunksize", prompt="chunk size", default=10000, type=int)
+@click.option("--target-table", prompt="table name", default="yellow_taxi_data")
+def run(year, month,
+        pg_user, pg_pass, pg_host, pg_port, pg_database,
+        chunksize, target_table):
+    """Download a single yellow taxi file and load it into Postgres."""
     prefix = 'https://github.com/DataTalksClub/nyc-tlc-data/releases/download/yellow/'
     url = prefix + f'yellow_tripdata_{year:04d}-{month:02d}.csv.gz'
-    engine = create_engine(f'postgresql+psycopg://{pg_user}:{pg_pass}@{pg_host}:{pg_port}/{pg_database}')
+    engine = create_engine(
+        f'postgresql+psycopg://{pg_user}:{pg_pass}@{pg_host}:{pg_port}/{pg_database}'
+    )
 
     df_iter = pd.read_csv(
         url,
@@ -55,16 +62,18 @@ def run():
     for df_chunk in tqdm(df_iter):
         if first:
             df_chunk.head(n=0).to_sql(
-                name=name, 
-                con=engine, 
-                if_exists='replace')
+                name=target_table,
+                con=engine,
+                if_exists='replace'
+            )
             first = False
-        
+
         df_chunk.to_sql(
-            name=name,
+            name=target_table,
             con=engine,
-            if_exists='append')
+            if_exists='append'
+        )
+
 
 if __name__ == '__main__':
     run()
-
