bf4bb5dfe26b2c012aaeb715bd645a694747028e
nandaReddy1212 <81372397+nandaReddy1212@users.noreply.github.com>
2026-03-13 04:17:19 +0000

    added dynamic load of data

--- pipeline/zones.py
+++ pipeline/zones.py
@@ -1,68 +1,38 @@
-#!/usr/bin/env python
-# coding: utf-8
-
-# In[1]:
-
-
 import pandas as pd
-
-
-# In[2]:
-
-
-get_ipython().system('uv add sqlalchemy')
-
-
-# In[5]:
-
-
-from sqlalchemy import create_engine
-engine = create_engine('postgresql+psycopg://root:root@localhost:5432/ny_taxi')
-
-
-# In[7]:
-
-
-df_zones = pd.read_csv('taxi_zone_lookup.csv')
-
-
-# In[12]:
-
-
-df_zones.head(n=0).to_sql(name='zones', con=engine, if_exists='replace')
-
-
-# In[22]:
-
-
-df_zones.count()
-
-
-# In[18]:
-
-
 from tqdm.auto import tqdm
-
-
-# In[24]:
-
-
-df_zones_iter = pd.read_csv(
-    'taxi_zone_lookup.csv',
-    iterator=True,
-    chunksize=100
-)
-
-
-# In[25]:
-
-
-for record in tqdm(df_zones_iter):
-    record.to_sql(name='zones' ,con=engine, if_exists='append')
-
-
-# In[ ]:
+from sqlalchemy import create_engine
+import click
 
 
 
+@click.command()
+@click.option("--pg-user", prompt="Postgres user", default="root")
+@click.option("--pg-pass", prompt="Postgres password", default="root")
+@click.option("--pg-host", prompt="Postgres host", default="localhost")
+@click.option("--pg-port", prompt="Postgres port", default=5432, type=int)
+@click.option("--pg-database", prompt="Postgres database", default="ny_taxi")
+@click.option("--chunksize", prompt="chunk size", default=100, type=int)
+@click.option("--target-table", prompt="table name", default="zones")
+def run(pg_user, pg_pass, pg_host, pg_port, pg_database, chunksize, target_table):
+    engine = create_engine(
+        f'postgresql+psycopg://{pg_user}:{pg_pass}@{pg_host}:{pg_port}/{pg_database}'
+    )
+    
+    df_zones_iter = pd.read_csv(
+        #'pipeline/taxi_zone_lookup.csv',
+        f'taxi_zone_lookup.csv',
+        iterator=True,
+        chunksize=chunksize
+    )
 
+    first = True
+    for record in tqdm(df_zones_iter):
+        if first:
+            record.head(n=0).to_sql(name=target_table, con=engine, if_exists='replace')
+            first = False
+            
+        record.to_sql(name=target_table, con=engine, if_exists='append')
+
+
+if __name__ == '__main__':
+    run()
